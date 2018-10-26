0c1be0a95f790c144ca6f3e16ebba4ec21617540
Janusch Jehle <JesJehle@gmx.de>
2018-10-26 10:40:26 +0200

    travis yml next

--- R/helpers.R
+++ R/helpers.R
@@ -55,41 +55,6 @@ is_type <- function(param, type) {
 
 
 
-#' find a folder in a speciefied subdirectory
-#' @param foldername the name of the folder to search
-#' @param root_dir the initial directory the to search
-#' @return the given folder with the path
-#' @noRd
-find_folder <- function(foldername, root_dir) {
-  directories <- list.dirs(path.expand(root_dir))
-  found_folder <- list("start")
-
-  for (folder in directories) {
-    if (foldername %in% dir(folder)) {
-      found_folder[length(found_folder)] <- file.path(folder, foldername)
-    }
-  }
-  return(found_folder)
-}
-
-#' find a file in a speciefied subdirectory
-#' @param filename the name of the file to search, with extension
-#' @param root_dir the initial directory the to search
-#' @return the given file with the path
-#' @noRd
-find_file <- function(filename, root_dir) {
-  directories <- list.dirs(path.expand(root_dir))
-  found_file <- list("start")
-  for (folder in directories) {
-    files <- list.files(folder)
-    if (filename %in% files) {
-      found_file[length(found_file)] <- file.path(folder, filename)
-    }
-  }
-  return(found_file)
-}
-
-
 #' get_name_from_path
 #' @param path A file path
 #' @return basename without extension
